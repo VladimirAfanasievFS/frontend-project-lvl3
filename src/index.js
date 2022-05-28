65d2efc77735d567a064a190f78c7c526687011b
Vladimir Afanasiev <vladimir.afanasiev@flatstack.dev>
2022-05-28 23:59:44 +0300

    init project

---
+++ src/index.js
@@ -0,0 +1,12 @@
+// @ts-check
+
+// import 'core-js/stable';
+// import 'regenerator-runtime/runtime';
+// стили подключаются тут, не в модуле init.js
+import './app.scss';
+// import run from './init.js';
+
+// // Только вызов, определения в другом файле
+// // https://ru.hexlet.io/blog/posts/skripty-moduli-i-biblioteki
+// // https://ru.hexlet.io/courses/js-frontend-architecture/lessons/initialization/theory_unit
+// run();
